a192f9e3f1b57fb049ffbbb83f56ff1473a9bffb
mcmoodoo <rashid.makhmud@gmail.com>
2026-04-28 09:43:06 -0400

    removed all nvim configs

--- nixos/nvim/lua/core/filetypes.lua
+++
@@ -1,8 +0,0 @@
--- Filetype detection overrides
--- DAML (Digital Asset Modeling Language) is Haskell-based; use Haskell syntax/indent/LSP
-vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
-  pattern = '*.daml',
-  callback = function()
-    vim.bo.filetype = 'haskell'
-  end,
-})
